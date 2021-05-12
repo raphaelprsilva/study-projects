1bff985c561b58194f399bf8e9e62747ce2a6966
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-12 10:13:27 -0300

    Create client class and instance them

--- 01-bite-bank/index.js
+++ 01-bite-bank/index.js
@@ -4,3 +4,20 @@ class Cliente {
   agencia;
   saldo;
 }
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Raphael";
+cliente1.cpf = 10115002642;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+
+cliente2.nome = "Danielle";
+cliente2.cpf = 10111124565;
+cliente2.agencia = 1002;
+cliente2.saldo = 0;
+
+console.log(cliente1);
+console.log(cliente2);
