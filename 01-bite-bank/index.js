9c31059bfcc7b20ab8559c0585ee0021366b066f
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-12 10:35:00 -0300

    Create sacar method

--- 01-bite-bank/index.js
+++ 01-bite-bank/index.js
@@ -1,23 +1,43 @@
 class Cliente {
   nome;
   cpf;
+}
+
+class ContaCorrente {
   agencia;
   saldo;
+
+  sacar(valor) {
+    if (this.saldo >= valor) {
+      this.saldo -= valor;
+    }
+  }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Raphael";
 cliente1.cpf = 10115002642;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 
+const cliente2 = new Cliente();
 cliente2.nome = "Danielle";
 cliente2.cpf = 10111124565;
-cliente2.agencia = 1002;
-cliente2.saldo = 0;
+
+const contaCorrenteRaphael = new ContaCorrente();
+contaCorrenteRaphael.saldo = 0;
+contaCorrenteRaphael.agencia = 1001;
 
 console.log(cliente1);
-console.log(cliente2);
+
+contaCorrenteRaphael.saldo = 100;
+console.log(contaCorrenteRaphael);
+
+contaCorrenteRaphael.sacar(200);
+
+
+
+const contaCorrenteDanielle = new ContaCorrente();
+contaCorrenteDanielle.saldo = 0;
+contaCorrenteDanielle.agencia = 1002;
+
+console.log(contaCorrenteRaphael);
