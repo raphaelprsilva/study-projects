a4e4bf691b14b718f359d0ad6086dae2ad9cba0a
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-12 11:38:31 -0300

    Remove Cliente and ContaCorrente classes to another files

--- 01-bite-bank/index.js
+++ 01-bite-bank/index.js
@@ -1,25 +1,3 @@
-class Cliente {
-  nome;
-  cpf;
-}
-
-class ContaCorrente {
-  agencia;
-  _saldo = 0;
-
-  sacar(valor) {
-    if (this._saldo >= valor) {
-      this._saldo -= valor;
-      return valor;
-    }
-  }
-
-  depositar(valor) {
-    if (valor <= 0) return;
-    this._saldo += valor;
-    return valor;
-  }
-}
 
 const cliente1 = new Cliente();
 cliente1.nome = "Raphael";
