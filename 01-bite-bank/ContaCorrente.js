7e1c611c00d948b99ddc47c0d6afeade32fa7d6e
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-13 09:38:15 -0300

    Add numeroDeContas property to count how much counts we have

--- 01-bite-bank/ContaCorrente.js
+++ 01-bite-bank/ContaCorrente.js
@@ -1,6 +1,7 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente {
+  static numeroDeContas = 0;
   agencia;
 
   _cliente;
@@ -24,6 +25,7 @@ export class ContaCorrente {
   constructor (agencia, cliente) {
     this.agencia = agencia;
     this.cliente = cliente;
+    ContaCorrente.numeroDeContas += 1;
   }
 
   sacar(valor) {
