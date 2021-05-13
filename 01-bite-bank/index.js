d0e69ad2c750ed22ae85433a45932e39a5832b7a
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-13 09:27:58 -0300

    Change code structure, using constructors created

--- 01-bite-bank/index.js
+++ 01-bite-bank/index.js
@@ -1,27 +1,13 @@
 import {Cliente} from '/home/raphael/Documentos/Raphael/Learning_Projects/study-projects/01-bite-bank/Cliente.js';
 import {ContaCorrente} from '/home/raphael/Documentos/Raphael/Learning_Projects/study-projects/01-bite-bank/ContaCorrente.js';
 
-const cliente1 = new Cliente();
-cliente1.nome = "Raphael";
-cliente1.cpf = 10115002642;
+const cliente1 = new Cliente('Raphael', 10115002642);
+const cliente2 = new Cliente('Danielle', 10111124565);
 
+const conta1 = new ContaCorrente(1001, cliente1);
+const conta2 = new ContaCorrente(1002, cliente2);
 
-// const cliente2 = new Cliente();
-// cliente2.nome = "Danielle";
-// cliente2.cpf = 10111124565;
+conta1.depositar(100);
+conta1.transferir(20, conta2);
 
-const contaCorrenteRaphael = new ContaCorrente();
-contaCorrenteRaphael.agencia = 1001;
-contaCorrenteRaphael.cliente = cliente1;
-contaCorrenteRaphael.depositar(100);
-
-
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente1;
-conta2.agencia = 1002;
-
-contaCorrenteRaphael.transferir(20, conta2);
-
-console.log(contaCorrenteRaphael);
-
-console.log(conta2._saldo);
+console.log(cliente1);
