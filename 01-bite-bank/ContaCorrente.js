c79e1ee4fb048e3184c331e855acb6d4179effbf
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-13 09:20:39 -0300

    Add constructor

--- 01-bite-bank/ContaCorrente.js
+++ 01-bite-bank/ContaCorrente.js
@@ -4,6 +4,7 @@ export class ContaCorrente {
   agencia;
 
   _cliente;
+  _saldo = 0;
 
   set cliente (novoValor) {
     if (novoValor instanceof Cliente) {
@@ -15,10 +16,14 @@ export class ContaCorrente {
     return this._cliente;
   }
 
-  _saldo = 0;
 
   get saldo() {
     return this._saldo;
+  }
+
+  constructor (agencia, cliente) {
+    this.agencia = agencia;
+    this.cliente = cliente;
   }
 
   sacar(valor) {
