53111bd54f536fdc977b00eeaef8c30401a8f2b5
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-14 08:05:07 -0300

    Change Conta type

--- 01-bite-bank/index.js
+++ 01-bite-bank/index.js
@@ -1,15 +1,13 @@
-import { Cliente } from "./Cliente.js"
-import { ContaCorrente } from "./ContaCorrente.js"
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Conta } from "./Conta.js";
 
 const cliente1 = new Cliente('Raphael', 11122233309);
 const cliente2 = new Cliente('Danielle', 88822233309);
 
-const conta1 = new ContaCorrente(1001, cliente1);
-const conta2 = new ContaCorrente(1002, cliente2);
+const conta1 = new Conta(50, cliente1, 1001);
+const conta2 = new Conta(100, cliente2, 1002);
 
-conta1.depositar(500);
-
-let valor = 200;
-conta1.transferir(valor, conta2);
-
+console.log(conta1);
 console.log(conta2);
