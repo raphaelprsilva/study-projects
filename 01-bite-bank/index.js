0d5398d1456d7c9cc9438db0f583a2db00650faf
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-14 08:25:16 -0300

    Change instace of contas

--- 01-bite-bank/index.js
+++ 01-bite-bank/index.js
@@ -6,8 +6,7 @@ import { Conta } from "./Conta.js";
 const cliente1 = new Cliente('Raphael', 11122233309);
 const cliente2 = new Cliente('Danielle', 88822233309);
 
-const conta1 = new Conta(50, cliente1, 1001);
-const conta2 = new Conta(100, cliente2, 1002);
+const conta1 = new ContaCorrente(1001, cliente1);
 
-console.log(conta1);
-console.log(conta2);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+console.log(contaPoupanca);
