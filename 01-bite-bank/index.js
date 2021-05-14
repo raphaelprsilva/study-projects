95720a5d735d39a908180e3d06d42f1f74e39415
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-14 07:45:02 -0300

    Add code to clients

--- 01-bite-bank/index.js
+++ 01-bite-bank/index.js
@@ -1,15 +1,15 @@
-import {Cliente} from '/home/raphael/Documentos/Raphael/Learning_Projects/study-projects/01-bite-bank/Cliente.js';
-import {ContaCorrente} from '/home/raphael/Documentos/Raphael/Learning_Projects/study-projects/01-bite-bank/ContaCorrente.js';
+import { Cliente } from "./Cliente.js"
+import { ContaCorrente } from "./ContaCorrente.js"
 
-const cliente1 = new Cliente('Raphael', 10115002642);
-const cliente2 = new Cliente('Danielle', 10111124565);
+const cliente1 = new Cliente('Raphael', 11122233309);
+const cliente2 = new Cliente('Danielle', 88822233309);
 
 const conta1 = new ContaCorrente(1001, cliente1);
 const conta2 = new ContaCorrente(1002, cliente2);
 
-conta1.depositar(100);
-conta1.transferir(20, conta2);
+conta1.depositar(500);
 
-console.log(conta1);
+let valor = 200;
+conta1.transferir(valor, conta2);
+
 console.log(conta2);
-console.log(ContaCorrente.numeroDeContas);
