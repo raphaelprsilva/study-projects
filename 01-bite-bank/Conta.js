00d752678765bbd825911cf2dc3238f88b570242
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-14 08:25:43 -0300

    Add getter and setters and methods

--- 01-bite-bank/Conta.js
+++ 01-bite-bank/Conta.js
@@ -5,10 +5,28 @@ export class Conta {
     this._agencia = agencia;
   }
 
+  set cliente (novoCliente) {
+    if (novoCliente instanceof Cliente) {
+      this._cliente = novoCliente;
+    }
+  }
+
+  get cliente () {
+    return this._cliente;
+  }
+
+
+  get saldo() {
+    return this._saldo;
+  }
+
   sacar(valor) {
-    if (this._saldo >= valor) {
-      this._saldo -= valor;
-      return valor;
+    let taxa = 1;
+    const valorSacado = taxa * valor;
+
+    if (this._saldo >= valorSacado) {
+      this._saldo -= valorSacado;
+      return valorSacado;
     }
   }
 
