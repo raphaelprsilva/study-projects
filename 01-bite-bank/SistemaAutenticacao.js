bba82e52d8e076bae0bba8de12ce8bfb4ceab2b6
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-15 11:34:10 -0300

    Check client and employee  athentication

--- 01-bite-bank/SistemaAutenticacao.js
+++ 01-bite-bank/SistemaAutenticacao.js
@@ -1,5 +1,16 @@
+/* 
+  Ser autenticável, significa ter o método 'senha'.
+*/
+
 export class SistemaAutenticacao {
-  static login(funcionario, senha) {
-    return funcionario.senha === senha;
+  static login(autenticavel, senha) {
+    if (SistemaAutenticacao.isAuthenticable(autenticavel)) {
+      return autenticavel.autenticar(senha);
+    }
+    return false;
+  }
+  static isAuthenticable(autenticavel) {
+    return 'autenticar' in autenticavel && 
+      autenticavel.autenticar instanceof Function;
   }
 }
