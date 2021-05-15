728ff339458fbe77ae45cfeee6bedb49a54c1e7e
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-15 10:22:05 -0300

    Do password authentication tests to manager and director

--- 01-bite-bank/index.js
+++ 01-bite-bank/index.js
@@ -1,8 +1,14 @@
 import { Cliente } from "./Cliente.js";
 import { Gerente } from "./Funcionarios/Gerente.js";
 import { Diretor } from "./Funcionarios/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor('Raphael', 2000, 11122233345);
-const gerente = new Gerente('Rodrigo', 1000, 11122233389);
+diretor.cadastrarSenha('123456789');
 
-SistemaAutenticacao.login(diretor, '123456789');
+const gerente = new Gerente('Rodrigo', 1000, 11122233389);
+gerente.cadastrarSenha('123465');
+
+const estaLogado = SistemaAutenticacao.login(gerente, '123456789');
+
+console.log(estaLogado);
