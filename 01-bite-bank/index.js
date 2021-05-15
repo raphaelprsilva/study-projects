3c08c644b20510db3cf805626caf3d064e38268d
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-15 09:38:48 -0300

    Verify new private method

--- 01-bite-bank/index.js
+++ 01-bite-bank/index.js
@@ -1,10 +1,14 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
-import { Conta } from "./Conta.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 const cliente1 = new Cliente('Raphael', 11122233309);
 const cliente2 = new Cliente('Danielle', 88822233309);
 
 const contaCorrente = new ContaCorrente(1001, cliente2);
 const contaPoupanca = new ContaPoupanca(100, cliente1, 1001);
+const contaSalarioRaphael = new ContaSalario(cliente1);
+
+contaSalarioRaphael.depositar(100);
+contaSalarioRaphael.sacar(50);
