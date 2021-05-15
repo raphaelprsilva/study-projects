05bdafa5ef1245b7941f82f5989b53198f7ee6f0
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-15 17:07:35 -0300

    Remove table line creation to another file

--- 02-doguito-petshop-project/service/client-service.js
+++ 02-doguito-petshop-project/service/client-service.js
@@ -1,29 +1,5 @@
-const criaNovaLinha = (nome, email, id) => {
-  const linhaNovoCliente = document.createElement('tr');
-  const conteduo = `
-    <td class="td" data-td>${nome}</td>
-    <td>${email}</td>
-    <td>
-        <ul class="tabela__botoes-controle">
-            <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
-            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-        </ul>
-    </td>`;
-  linhaNovoCliente.innerHTML = conteduo;
-  return linhaNovoCliente;
-}
-
-const tabela = document.querySelector('[data-tabela]');
-
 const listaClientes = () => {
   const url = 'http://localhost:3000/profile';
   return fetch(url)
     .then(response => response.json());
 }
-
-listaClientes()
-  .then( data => {
-    data.forEach(element => {
-      tabela.appendChild(criaNovaLinha(element.nome, element.email));
-    });
-  });
