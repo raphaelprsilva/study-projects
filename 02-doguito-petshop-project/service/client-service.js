87fdbc1290a0b8e2483030796687574cd811cff6
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-17 09:14:43 -0300

    Add new request to create new client

--- 02-doguito-petshop-project/service/client-service.js
+++ 02-doguito-petshop-project/service/client-service.js
@@ -4,6 +4,23 @@ const listaClientes = () => {
     .then(response => response.json());
 }
 
+const criaCliente = (nome, email) => {
+  return fetch('http://localhost:3000/profile', {
+    method: 'POST',
+    headers: {
+      'Content-type' : 'application/json'
+    },
+    body: JSON.stringify({
+      nome: nome,
+      email: email
+    })
+  })
+    .then(resposta => {
+      return resposta.body;
+    });
+};
+
 export const clienteService = {
   listaClientes,
+  criaCliente,
 };
