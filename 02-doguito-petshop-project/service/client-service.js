bb16c031482f8ad630b5241dd0bf9ba75e7eb980
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-18 08:50:56 -0300

    Add comments to explain how fetch works to create new client

--- 02-doguito-petshop-project/service/client-service.js
+++ 02-doguito-petshop-project/service/client-service.js
@@ -5,6 +5,8 @@ const listaClientes = () => {
 }
 
 const criaCliente = (nome, email) => {
+  // fetch(url, método) -> Neste caso, vamos utilizar o método POST
+  // pois, por padrão, o fetch usa o método GET
   return fetch('http://localhost:3000/profile', {
     method: 'POST',
     headers: {
