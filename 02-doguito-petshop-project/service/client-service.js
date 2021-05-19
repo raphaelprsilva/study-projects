4b6b6b1a59f414842dab7af289ca2163bd564468
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-19 20:15:47 -0300

    Add content

--- 02-doguito-petshop-project/service/client-service.js
+++ 02-doguito-petshop-project/service/client-service.js
@@ -22,7 +22,14 @@ const criaCliente = (nome, email) => {
     });
 };
 
+const removeCliente = (id) => {
+  return fetch(`http://localhost:3000/profile/${id}`, {
+    method: 'DELETE'
+  })
+};
+
 export const clienteService = {
   listaClientes,
   criaCliente,
+  removeCliente,
 };
