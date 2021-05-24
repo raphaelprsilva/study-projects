882713dc4f41effe28e112277786bb5ffe6b05fe
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-24 20:43:26 -0300

    Add CardNotas Component

--- 03-creep-project/ceep/src/components/ListaDeNotas.jsx
+++ 03-creep-project/ceep/src/components/ListaDeNotas.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
+import CardNotas from './CardNotas';
 
 export default class ListaDeNotas extends Component{
   render() {
     return (
       <ul>
-        <li> 
-          <section>
-            <header>
-              <h3>Título</h3>
-            </header>
-            <p>Escreva sua nota</p>
-          </section>
+        <li>
+        <CardNotas/>
+        </li>
+        <li>
+        <CardNotas/>
         </li>
       </ul>
     );
