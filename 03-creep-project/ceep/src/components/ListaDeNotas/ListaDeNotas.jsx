6c286346e8da4c575780cb0cbc4d4f2b5fdf5c3a
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-25 13:14:26 -0300

    Add import from style.css and add className to  ul and li items

--- 03-creep-project/ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ 03-creep-project/ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import CardNotas from '../CardNota/CardNotas';
+import './style.css';
 
 class ListaDeNotas extends Component{
   render() {
     return (
-      <ul>
+      <ul className="lista-notas">
         {Array
           .of('Trabalho', 'Trabalho', 'Estudos')
           .map((categoria, index) => {
             return (
-              <li key={ index }>
+              <li className="lista-notas_item" key={ index }>
                 <span>{ categoria }</span>
-                <CardNotas/>
+                <CardNotas />
               </li>
             );
           })
