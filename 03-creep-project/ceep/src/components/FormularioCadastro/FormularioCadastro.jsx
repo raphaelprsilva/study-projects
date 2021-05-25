654bf09c9a03d1e91e269ff210b3ea3dbe66e0a8
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-25 13:10:33 -0300

    Add FormularioCard component with his style.css

---
+++ 03-creep-project/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class FormularioCadastro extends Component {
+  render() {
+    return(
+      <form>
+        <input 
+        type="text" 
+        placeholder="Título"
+        />
+        <textarea placeholder="Escreva sua nota..."></textarea>
+        <button>Criar Nota</button>
+      </form>
+    );
+  }
+}
+
+export default FormularioCadastro;
