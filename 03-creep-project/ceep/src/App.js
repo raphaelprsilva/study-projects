4f6f7d92f2d6a27bc294e82b1298a00d4f450921
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-25 13:06:30 -0300

    Import App.css

--- 03-creep-project/ceep/src/App.js
+++ 03-creep-project/ceep/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ListaDeNotas from './components/ListaDeNotas/ListaDeNotas';
-import FormularioCadastro from './components/FormularioCadastro';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import '../src/App.css';
 
 class App extends Component {
   render() {
